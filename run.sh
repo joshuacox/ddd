a78c1aeafdf3b00647ff2cb399254a5df7d418cc
Josh Cox <josh@webhosting.coop>
2015-11-19 17:32:23 -0600

    new era

--- run.sh
+++
@@ -1,5 +0,0 @@
-#/bin/sh
-echo 'Starting Aegir-docker-debian'
-docker run -P ddd &
-sleep 5
-docker ps
--- showme.sh
+++
@@ -1,5 +0,0 @@
-#/bin/sh
-echo 'List of Containers with ports'
-sleep 1
-ADDRESS=$(docker ps -q|sort |xargs -n1 -I '{}' docker port {} 80)
-for i in $ADDRESS; do echo "http://$i"; chromium $i & done
