9364138653e686e0ccb70de0a5ea4e645b280a85
Josh Cox <josh@webhosting.coop>
2016-07-30 14:56:11 -0500

    updateInstalldrush.sh

--- installdrush.sh
+++ installdrush.sh
@@ -1,12 +1,12 @@
 # Download latest stable release using the code below or browse to github.com/drush-ops/drush/releases.
-wget http://files.drush.org/drush.phar
+php -r "readfile('http://files.drush.org/drush.phar');" > drush
 
 # Test your install.
-php drush.phar core-status
+php drush core-status
 
 # Rename to `drush` instead of `php drush.phar`. Destination can be anywhere on $PATH. 
-chmod +x drush.phar
-mv drush.phar /usr/local/bin/drush
+chmod +x drush
+mv drush /usr/local/bin/drush
 
 # Enrich the bash startup file with completion and aliases.
 drush init
