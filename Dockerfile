cbdc00652d92964dc6fa633471a58678e79956ae
Josh Cox <josh@webhosting.coop>
2019-02-06 18:18:10 -0600

    Added drush and composer back in

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,18 @@
 FROM drupal:8
 
 RUN apt-get update
-RUN apt-get install -y wget mysql-client
-RUN rm -rf /var/lib/apt/lists/*
+RUN apt-get install -y wget mysql-client git \
+ && rm -rf /var/lib/apt/lists/*
 
-#COPY installdrush.sh /root/installdrush.sh
-#RUN /bin/bash /root/installdrush.sh
-#RUN chsh -s /bin/bash www-data
+COPY installcomposer.sh /root/installcomposer.sh
+COPY installdrush.sh /root/installdrush.sh
+RUN /root/installcomposer.sh
+RUN chsh -s /bin/bash www-data \
+ && mkdir -p /var/www/.composer/cache \
+ && chown -R www-data. /var/www/.composer \
+ && chown -R www-data. /var/www/html \
+ && /root/installdrush.sh
+WORKDIR  /var/www/html
+USER www-data
+RUN composer require drush/drush
+USER root
--- installdrush.sh
+++ installdrush.sh
@@ -1,25 +1,16 @@
-# Download latest stable release using the code below or browse to github.com/drush-ops/drush/releases.
-php -r "readfile('http://files.drush.org/drush.phar');" > drush
-
-# Test your install.
-php drush core-status
-
+#!/bin/sh
+TMP=$(mktemp -d)
+cd $TMP
+wget -q -O drush.phar https://github.com/drush-ops/drush-launcher/releases/download/0.6.0/drush.phar
 # Rename to `drush` instead of `php drush.phar`. Destination can be anywhere on $PATH. 
-chmod +x drush
-mv drush /usr/local/bin/drush
+chmod +x drush.phar
+mv drush.phar /usr/local/bin/drush
 
 # Enrich the bash startup file with completion and aliases.
-drush init
+#drush init
 
-# composer
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('SHA384', 'composer-setup.php') === 'e115a8dc7871f15d853148a7fbac7da27d6c0030b848d9b3dc09e2a0388afed865e6a3d6b3c0fad45c48e2b5fc1196ae') { echo 'Installer verified';  } else { echo 'Installer corrupt'; unlink('composer-setup.php');  } echo PHP_EOL;"
-php composer-setup.php
-php -r "unlink('composer-setup.php');"
-mv /var/www/html/composer.phar /usr/local/bin/composer
-
-rm -Rf /var/www/html
-cd /var/www
-drush dl drupal-8
-mv drupal-8.* html
-chown -R www-data. /var/www
+#rm -Rf /var/www/html
+#cd /var/www
+#drush dl drupal-8
+#mv drupal-8.* html
+#chown -R www-data. /var/www
